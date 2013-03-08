00472ef171eff3ae749e1cff731e088361bc5b78
Mateus Gomes <mateusg.18@gmail.com>
2013-03-08 15:35:08 -0300

    Move campaign to /apns

---
+++ lib/rapns/apns/campaign.rb
@@ -0,0 +1,13 @@
+module Rapns
+  module Apns
+    class Campaign < Rapns::Base
+      self.table_name = 'rapns_campaigns'
+
+      belongs_to :app, :class_name => "Rapns::App"
+
+      attr_accessible :name, :description, :app_id
+
+      validates_presence_of :name
+    end
+  end
+end
