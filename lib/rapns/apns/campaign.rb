5836981944e4f7ae26130533c728578c8779e23b
Mateus Gomes <mateusg.18@gmail.com>
2013-03-19 20:27:08 -0300

    Add campaign_ids and action_types custom accessors.

--- lib/rapns/apns/campaign.rb
+++ lib/rapns/apns/campaign.rb
@@ -8,6 +8,24 @@ module Rapns
       attr_accessible :name, :description, :app_id, :badge, :alert, :action_types, :catalog_ids
 
       validates_presence_of :name, :app_id
+
+      def catalog_ids=(ids)
+        ids.select!(&:present?) if ids.is_a? Array
+        write_attribute(:catalog_ids, ids)
+      end
+
+      def catalog_ids
+        read_attribute(:catalog_ids).split(',')
+      end
+
+      def action_types=(action_types)
+        action_types.select!(&:present?) if action_types.is_a? Array
+        write_attribute(:action_types, action_types)
+      end
+
+      def action_types
+        read_attribute(:action_types).split(',')
+      end
     end
   end
 end
