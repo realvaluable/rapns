aa49749791a68047a8cfc8041a4f28ce0ebc7d54
Brenno Costa <brennolncosta@gmail.com>
2013-03-20 16:49:27 -0300

    Use AR::AM::Serialization instead modify accessors

--- lib/rapns/apns/campaign.rb
+++ lib/rapns/apns/campaign.rb
@@ -3,29 +3,14 @@ module Rapns
     class Campaign < Rapns::Base
       self.table_name = 'rapns_campaigns'
 
+      serialize :catalog_ids
+      serialize :action_types
+
       belongs_to :app, :class_name => "Rapns::App"
 
       attr_accessible :name, :description, :app_id, :badge, :alert, :action_types, :catalog_ids
 
       validates_presence_of :name, :app_id
-
-      def catalog_ids=(ids)
-        ids.select!(&:present?) if ids.is_a? Array
-        write_attribute(:catalog_ids, ids)
-      end
-
-      def catalog_ids
-        read_attribute(:catalog_ids).split(',')
-      end
-
-      def action_types=(action_types)
-        action_types.select!(&:present?) if action_types.is_a? Array
-        write_attribute(:action_types, action_types)
-      end
-
-      def action_types
-        read_attribute(:action_types).split(',')
-      end
     end
   end
 end
