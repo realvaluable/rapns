43d6fb7233987c8a875927db807ed09b3ece1b18
Mateus Gomes <mateusg.18@gmail.com>
2013-03-22 19:45:49 -0300

    Use activesupport/concern helpers.

--- lib/rapns/concerns/rapns_custom_connection.rb
+++ lib/rapns/concerns/rapns_custom_connection.rb
@@ -1,9 +1,7 @@
 module RapnsCustomConnection
   extend ActiveSupport::Concern
 
-  def self.included(base)
-    base.class_eval do
-      establish_connection "apns_#{Rails.env}"
-    end
+  included do
+    establish_connection "apns_#{Rails.env}"
   end
 end
